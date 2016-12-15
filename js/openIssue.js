6d3e076a2b8da4170259f4d38ce003d656e11795
Lucas Whitman <lwhitman@levelsbeyond.com>
2016-12-14 20:24:49 -0700

    add context menu functionality, refactor code a bit

--- js/openIssue.js
+++ js/openIssue.js
@@ -1,114 +1,60 @@
+const ENTER = 13;
+
 function setErrorText(errorText) {
-	var divError = document.getElementById('error');
-	divError.innerText = errorText + '\n';
+  var divError = document.getElementById('error');
+  divError.innerText = errorText + '\n';
 }
 
 function colorBorderToRed(element) {
-	var border = document.getElementById(element);
-	border.style.borderColor = '#FF6666';
-	border.focus();
-	border.select();
+  var border = document.getElementById(element);
+  border.style.borderColor = '#FF6666';
+  border.focus();
+  border.select();
 }
 
 function returnError(errorText, element) {
-	setErrorText(errorText);
-	colorBorderToRed(element);
-	throw '';
+  setErrorText(errorText);
+  colorBorderToRed(element);
+  throw '';
 }
-
-var key,
-		url;
-
-function setUrl(urlOption) {
-  if (urlOption !== undefined) {
-  	if (urlOption.charAt(urlOption.length - 1) === '/') {
-    	url = urlOption + 'browse/';
-    } else {
-      url = urlOption + '/browse/';
-    }
-  }
-}
-
-function removeSpaces(string) {
-  while (string.charAt(string.length - 1) === ' ') {
-		string = string.slice(0, string.length - 1);
-	}
-
-	if (string.charAt(0) === ' ') {
-		var temp = string.split(' ');
-		string = temp[temp.length - 1];
-	}
-
-	return string;
-}
-
-function removeSkypeFormatting(string) {
-	if (string.charAt(0) === '[') {
-		var temp = string.split(' ');
-		string = temp[temp.length - 1];
-	}
-
-	return string;
-}
-
-function addDefaultProject(string, project) {
-  if (string.match(/^[0-9]+$/) != null) {
-    string = project + '-' + string;
-  }
-
-  return string;
-}
-
-function openWindow() {
-	window.open(url + key);
-}
-
-function openIssue() {
-	chrome.storage.sync.get(function (item) {
-
-		var urlOption = item.savedUrl;
-    var projectOption = item.savedProject;
-
-		key = document.getElementById('key').value;
-		setUrl(urlOption);
-		key = removeSpaces(key);
-		key = removeSkypeFormatting(key);
-    key = addDefaultProject(key, projectOption);
-
-		if (key === '') {
-			returnError('Please insert key', 'key');
-		} else if (url === undefined) {
-			returnError('Please define URL in Options', 'key');
-    } else {
-			openWindow();
-		}
-	});
-}
-
-var enter = 13;
 
 function inputKeyListener(e) {
-	if (e.keyCode === enter) {
-		openIssue();
-	}
+  if (e.keyCode === ENTER) {
+    var key = document.getElementById('key').value;
+    sendOpenIssueMessage(key);
+  }
 }
 
 function listenInputKey(inputKey) {
-	if (inputKey.addEventListener) {
-		inputKey.addEventListener('keydown', inputKeyListener, false);
-	} else if (inputKey.attachEvent) {
-		inputKey.attachEvent('keydown', inputKeyListener);
-	}
+  if (inputKey.addEventListener) {
+    inputKey.addEventListener('keydown', inputKeyListener, false);
+  } else if (inputKey.attachEvent) {
+    inputKey.attachEvent('keydown', inputKeyListener);
+  }
 }
 
 function listenKeys() {
-	listenInputKey(document.getElementById('key'));
+  listenInputKey(document.getElementById('key'));
 }
 
 if (window.addEventListener) {
-	window.addEventListener('load', listenKeys, false);
+  window.addEventListener('load', listenKeys, false);
 } else if (window.attachEvent) {
-	window.attachEvent('onload', listenKeys);
+  window.attachEvent('onload', listenKeys);
 } else {
-	document.addEventListener('load', listenKeys, false);
+  document.addEventListener('load', listenKeys, false);
 }
+
+// send message to background
+function sendOpenIssueMessage(key) {
+  chrome.extension.sendRequest({ msg: "openIssue", data:key });
+}
+
+// listener from background
+chrome.extension.onRequest.addListener(
+    function(request, sender, sendResponse){
+        if(request.msg == 'error') {
+          returnError(request.data);
+        }
+    }
+);
